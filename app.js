fd3603c9e265a543b727f3aa835a3fc09223cfe6
vnosikov <vasily-nosikov@yandex.ru>
2015-11-13 17:49:10 -0300

    balls movement base

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-	prepare();
+	prepare();	
 	handleNewTurn();
+	$('.container').click(function(e){
+		onClick(e, $(this)[0]);
+	});
 });
+
+const CELL_SIZE = 50;
+const BALL_SIZE = 30;
+const BALL_OFFSET = (CELL_SIZE - BALL_SIZE)/2;
+
+var ballActive=null;
 
 function handleNewTurn(){
 	makeNewTurn();
@@ -9,7 +18,7 @@ function handleNewTurn(){
 		for(var j=0; j<9; j++){
 			var color = getBallFromTable({x:i, y:j});
 			if(color != 0){
-				var ball = $("<div class='ball'>" + color + "</div>");
+				var ball = $("<div class='ball ball-" + i + "-"+ j + "'>" + color + "</div>");
 				var pos = indexToPos({x: i, y:j});
 				ball.css('top', pos.y);
 				ball.css('left', pos.x);
@@ -19,10 +28,70 @@ function handleNewTurn(){
 	}
 }
 
+function onClick(e, cont){
+	var relX = e.pageX - cont.offsetLeft;
+    var relY = e.pageY - cont.offsetTop;
+	
+	var index = posToIndex({x:relX, y:relY});
+	
+	if(ballActive == null){
+		if(getBallFromTable(index) != 0){
+			ballActive = index;
+			getBallByIndex(index).addClass('active');
+		}
+	}
+		
+	else{
+		if(getBallFromTable(index) == 0){
+			var path = lookForAPath(ballActive, index);
+			if(path.length!=0){
+				//TODO:Add moving animation
+				var oldPos = getBallIndexClassNameByIndex(ballActive);
+				var newPos = getBallIndexClassNameByIndex(index);
+				var ball = getBallByIndex(ballActive);
+				ball.removeClass(oldPos);
+				ball.addClass(newPos);
+				
+				var pos = indexToPos(index);
+				ball.css('top', pos.y);
+				ball.css('left', pos.x);
+				
+				moveBall(ballActive, index);
+				
+				ball.removeClass('active');
+				ballActive = null;
+			}
+		}
+		else{
+			getBallByIndex(ballActive).removeClass('active');
+			ballActive = index;
+			getBallByIndex(index).addClass('active');
+		}
+	}
+	
+	//handleNewTurn();
+}
+
 function indexToPos(index){
 	var pair={
-		x: index.x * 50 + 10,
-		y: index.y * 50 + 10
+		x: index.x * CELL_SIZE + BALL_OFFSET,
+		y: index.y * CELL_SIZE + BALL_OFFSET
 	}
 	return pair;
+}
+
+function posToIndex(pos){
+	var pair = {
+		x: Math.floor(pos.x/CELL_SIZE),
+		y: Math.floor(pos.y/CELL_SIZE)
+	}
+	return pair;
+}
+
+function getBallByIndex(index){
+	return $('.ball-' + index.x + '-' + index.y);
+}
+
+function getBallIndexClassNameByIndex(index){
+	return 'ball-' + index.x + '-' + index.y;
 }
--- lines.js
+++ lines.js
@@ -43,11 +43,16 @@ function generateRandomColor(){
 }
 
 function createNewBall(color){
-	var index = Math.floor(Math.random()*emptyFields.length);
-	var pair = emptyFields[index];
-	emptyFields.splice(index, 1);
+	if(emptyFields.length>0){
+		var index = Math.floor(Math.random()*emptyFields.length);
+		var pair = emptyFields[index];
+		emptyFields.splice(index, 1);
 	
-	tableField[pair.x][pair.y] = color;
+		tableField[pair.x][pair.y] = color;
+	}
+	else{
+		throw new Error('Impossible to create new ball when the field is full');
+	}
 }
 
 function moveBall(oldLocation, newLocation){
@@ -69,4 +74,128 @@ function makeNewTurn(){
 	}
 	
 	nextColors = [generateRandomColor(), generateRandomColor(), generateRandomColor()];
+}
+
+function lookForAPath(source, target){
+	var tags = [];
+	for(var i=0; i<9;i++){
+		var line=[];
+		for(var j=0; j<9;j++){
+			if(i == source.x && j == source.y){
+				line.push(0);
+			}
+			else{
+				line.push(-1);
+			}
+		}
+		tags.push(line);
+	}
+	
+	var step=0;
+	var oldFront = [{x:source.x, y:source.y}];
+	var newFront = [];
+	var adjs = [];
+	var found = false, noWay = false;
+	while(!found && !noWay){
+		adjs=[];
+		for (var i=0;i<oldFront.length;i++){
+			var pair = oldFront[i];
+			adjs = adjs.concat(getAdjacentPairs(pair));
+		}
+		for(i=0;i<adjs.length;i++){
+			pair = adjs[i]
+			if(tags[pair.x][pair.y] == -1){
+				tags[pair.x][pair.y] = step + 1;
+				newFront.push(pair);
+			}
+		}
+		
+		if(newFront.length == 0){
+			noWay = true;
+		}
+		
+		else if(contains(newFront, target)){
+			found = true;
+		}
+		
+		else{
+			oldFront = newFront;
+			newFront = [];
+			step++;
+		}
+	}
+	
+	if(noWay){
+		console.log('No way found');
+		return [];
+	}
+	else{
+		var path = [target];
+		var lastPoint = target;
+		while(true){
+			if(step==0){
+				console.log("Path from {" + source.x + " " + source.y + "} to {" + target.x + " " + target.y +"}");
+				var s ="";
+				for(var k=0;k<path.length;k++){
+					s += " {" + path[k].x + " " + path[k].y + "},";
+				}
+				console.log(s);
+				return path;
+			}
+			
+			var adjs = getAdjacentPairs(lastPoint);
+			for (var i=0; i<adjs.length;i++){
+				var p = adjs[i];
+				if(tags[p.x][p.y] == step){
+					path.unshift(p);
+					step--;
+					lastPoint = p;
+					break;
+				}
+			}
+			
+			if(i >= adjs.length){
+				throw new Error("Can't restore a path between two points");
+			}
+		}
+	}
+	
+	function contains(a, obj) {
+		for (var i = 0; i < a.length; i++) {
+			if (a[i].x == obj.x && a[i].y == obj.y) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+function getAdjacentPairs(pair){
+	var result=[];
+	if(pair.x>0){
+		var p = {x:pair.x-1, y:pair.y};
+		if(getBallFromTable(p) == 0){
+			result.push(p);
+		}
+	}
+	if(pair.x<8){
+		var p = {x:pair.x+1, y:pair.y};
+		if(getBallFromTable(p) == 0){
+			result.push(p);
+		}
+	}
+	if(pair.y>0){
+		var p = {x:pair.x, y:pair.y-1};
+		if(getBallFromTable(p) == 0){
+			result.push(p);
+		}
+	}
+	if(pair.y<8){
+		var p = {x:pair.x, y:pair.y+1};
+		if(getBallFromTable(p) == 0){
+			result.push(p);
+		}
+	}
+	
+	return result;
 }
